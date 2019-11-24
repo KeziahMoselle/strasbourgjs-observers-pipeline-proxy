e6513a78fd31a86840092dcba255add6baf1359d
KeziahMoselle <keziah.moselle@gmail.com>
2019-11-24 17:14:19 +0100

    feat: subscribe and unsubscribe button

--- src/index.js
+++ src/index.js
@@ -3,19 +3,54 @@ import Subject from './Subject.js'
 // Create a new instance of Observable
 const subject = new Subject()
 
-// Declare DOM elements
-const usernameInput = document.getElementById('usernameInput')
-const usernameValue = document.getElementById('usernameValue')
+// Loop through all our paragraphs
+for (const observerElement of document.querySelectorAll('.observer')) {
+  // Create a unique function
+  function update (value) {
+    observerElement.querySelector('p').textContent = value
+  }
 
-// Utility function
-function updateValue (value) {
-  usernameValue.textContent = value
+  // Subscribe the function
+  subject.subscribe(update)
+
+  // Click on the subscribe btn
+  observerElement.querySelector('.btn-subscribe')
+    .addEventListener('click', () => {
+      subject.subscribe(update)
+    })
+
+  // Click on the unsubscribe btn
+  observerElement.querySelector('.btn-unsubscribe')
+    .addEventListener('click', () => subject.unsubscribe(update))
 }
 
-// Subscribe to the event
-subject.subscribe(updateValue)
-
 // Listen to the event and notify all subscribers
-usernameInput.addEventListener('keyup', (event) => {
-  subject.notify(event.target.value)
-})
+document.getElementById('usernameInput')
+  .addEventListener('keyup', event => subject.notify(event.target.value))
+
+
+
+
+  
+  /*=============================================
+  =            Styling purpose                  =
+  =============================================*/
+
+// Loop through all our paragraphs
+for (const observerElement of document.querySelectorAll('.observer')) {
+  observerElement.classList.add('on')
+
+  // Click on the subscribe btn
+  observerElement.querySelector('.btn-subscribe')
+    .addEventListener('click', () => {
+      observerElement.classList.add('on')
+      observerElement.classList.remove('off')
+    })
+
+  // Click on the unsubscribe btn
+  observerElement.querySelector('.btn-unsubscribe')
+    .addEventListener('click', () => {
+      observerElement.classList.add('off')
+      observerElement.classList.remove('on')
+    })
+}
